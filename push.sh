7ab2f1cff07c3cab51728d1fcc1c482ef9aada71
40huo <git@40huo.cn>
2017-10-22 16:21:24 +0800

    添加travis

---
+++ push.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+# Push HTML files to gh-pages automatically.
+
+# Fill this out with the correct org/repo
+ORG=ctf-wiki
+REPO=ctf-wiki
+# This probably should match an email for one of your users.
+EMAIL=git@40huo.cn
+
+set -e
+
+# Clone the gh-pages branch outside of the repo and cd into it.
+cd ..
+git clone -b gh-pages "https://$GH_TOKEN@github.com/$ORG/$REPO.git" gh-pages
+cd gh-pages
+
+# Update git configuration so I can push.
+if [ "$1" != "dry" ]; then
+    # Update git config.
+    git config user.name "Travis Builder"
+    git config user.email "$EMAIL"
+fi
+
+# Copy in the HTML.  You may want to change this with your documentation path.
+cp -R ../$REPO/_build/html/* ./
+
+# Add and commit changes.
+git add -A .
+git commit -m "[ci skip] Autodoc commit for $COMMIT."
+if [ "$1" != "dry" ]; then
+    # -q is very important, otherwise you leak your GH_TOKEN
+    git push -q origin gh-pages
+fi
