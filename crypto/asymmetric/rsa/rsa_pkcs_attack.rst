0099670b314773464afcc65edfb25504e5131da9
40huo <git@40huo.cn>
2017-10-22 16:22:27 +0800

    Initial gh-pages commit.

--- crypto/asymmetric/rsa/rsa_pkcs_attack.rst
+++
@@ -1,8 +0,0 @@
-Bleichenbacher's attack
-=======================
-
--  PKCS 1.5标准中可以伪造rsa签名
-
-待补充。
-
--  http://ddaa.tw/gctf\_crypto\_201\_rsa\_ctf\_challenge.html
--- pwn/fmtstr/md2rst.py
+++
@@ -1,6 +0,0 @@
-import os
-for f in os.listdir('./'):
-    if os.path.isfile(f) and f.endswith('.md'):
-        cmd = 'pandoc --columns=50  --from markdown --to rst -s ' + f + ' -o ' + f[:
-                                                                                   -3] + '.rst'
-        os.system(cmd)
