19cb085fded8024650e2f82451efdba737c91859
40huo <git@40huo.cn>
2017-11-14 15:25:08 +0800

    更正rm

--- push.sh
+++ push.sh
@@ -22,7 +22,7 @@ if [ "$1" != "dry" ]; then
 fi
 
 # Copy in the HTML.  You may want to change this with your documentation path.
-rm -rf .
+rm -rf *
 cp -R ../$REPO/_build/html/* ./
 
 # Add and commit changes.
