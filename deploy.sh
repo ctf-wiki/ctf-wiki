5890ce628445ebd48fd7ccda78d9d961a08d915c
40huo <git@40huo.cn>
2018-02-13 23:55:45 +0800

    切换到主仓库

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 # Push HTML files to gh-pages automatically.
 
 # Fill this out with the correct org/repo
-ORG=40huo
+ORG=ctf-wiki
 REPO=ctf-wiki
 # This probably should match an email for one of your users.
 EMAIL=git@40huo.cn
